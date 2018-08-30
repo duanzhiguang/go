8962f592811fe06e67fcb8e25a003a4af3dc64b1
duanzhiguang <294604621@qq.com>
2018-08-30 15:03:35 +0800

    初始beego

--- BeeGoCode/src/DzGoProject/routers/router.go
+++ BeeGoCode/src/DzGoProject/routers/router.go
@@ -7,7 +7,7 @@ import (
 
 func init() {
 	beego.Router("/", &controllers.MainController{})
-	beego.Router("/user/?id", &controllers.UserController{}, "get:GetInfo")
+	beego.Router("/user/?:id", &controllers.UserController{}, "get:GetInfo")
 	beego.Router("/user/name/?:name", &controllers.UserController{}, "get:GetUserName")
 	beego.Router("/user/*.*", &controllers.UserController{}, "get:GetFileType")
 
